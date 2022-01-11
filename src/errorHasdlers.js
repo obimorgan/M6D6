d72601185f6b9b2a423c4e8edb4d3e8a2e86d023
Roby Morgan <roby.c.morgan@gmail.com>
2022-01-11 16:31:21 +0100

    added http errors

--- src/errorHasdlers.js
+++ src/errorHasdlers.js
@@ -1,1 +1,32 @@
 /** @format */
+
+export const notFoundHandler = (err, req, res, next) => {
+  if (err.status === 404) {
+    res.status(404).send({ message: err.message });
+  } else {
+    next(err);
+  }
+};
+
+export const badRequestHandler = (err, req, res, next) => {
+  console.log(err.name);
+  if (err.status === 400 || err.name === "ValidationError") {
+    res.status(400).send({ message: err.errors || "Bad Request" });
+  } else {
+    next(err);
+  }
+};
+
+export const genericErrorHandler = (err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ message: "Generic Server Error" });
+};
+
+export const syntaxError = (err, req, res, next) => {
+  console.log(err.name);
+  if (err.status === 400 || err.name === "SyntaxError") {
+    res.status(400).send({ message: err.errors || "SyntaxError" });
+  } else {
+    next(err);
+  }
+};
--- src/services/blogs/index.js
+++ src/services/blogs/index.js
@@ -1,6 +1,7 @@
 /** @format */
 import express from "express";
 import BlogModel from "./schema.js";
+import createHttpError from "http-errors";
 
 const blogsRouter = express.Router();
 
@@ -31,7 +32,13 @@ blogsRouter
     try {
       const blogId = req.params.blogId;
       const blog = await BlogModel.findById(blogId);
-      res.status(200).send(blog);
+      if (editBlog) {
+        res.status(201).send(blog);
+      } else {
+        next(
+          createHttpError(404, "Blog with this id:", blogId, "is not found")
+        );
+      }
     } catch (error) {
       console.log(error);
       next(error);
@@ -43,17 +50,29 @@ blogsRouter
       const editBlog = await BlogModel.findByIdAndUpdate(blogId, req.body, {
         new: true,
       });
-      res.status(201).send(editBlog);
+      if (editBlog) {
+        res.status(201).send(editBlog);
+      } else {
+        next(
+          createHttpError(404, "Blog with this id:", blogId, "is not found")
+        );
+      }
     } catch (error) {
       console.log(error);
       next(error);
     }
   })
   .delete(async (req, res, next) => {
+    console.log("hello");
     try {
       const blogId = req.params.blogId;
       const deleteBlog = await BlogModel.findByIdAndDelete(blogId);
-      res.status(204).send(deleteBlog);
+      console.log(deleteBlog);
+      if (deleteBlog) {
+        res.status(204).send("Deleted blog", deleteBlog);
+      } else {
+        next(createHttpError(400, "SyntaxError"));
+      }
     } catch (error) {
       console.log(error);
       next(error);
