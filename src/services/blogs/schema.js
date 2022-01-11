269f5a20c1dd888e4897052afb32fd0ecae89a44
Roby Morgan <roby.c.morgan@gmail.com>
2022-01-11 14:21:54 +0100

    Added blog schema.

--- src/services/blogs/schema.js
+++ src/services/blogs/schema.js
@@ -1,1 +1,27 @@
 /** @format */
+
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const blogSchema = new Schema(
+  {
+    category: { types: String, required: true },
+    title: { types: String, required: true },
+    cover: { types: String, required: true },
+    readTime: {
+      value: { type: Number, required: true },
+      unit: { type: String, default: "min" },
+    },
+    author: {
+      name: { types: String, required: true },
+      avatar: { types: String, required: true },
+    },
+    content: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default model("Blog", blogSchema);
